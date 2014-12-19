896510d98ee4298a7976e4afdca463d1006469b9
Seandon Mooy <seandon.mooy@gmail.com>
2014-12-19 01:11:56 -0600

    stats, better logging

--- cloudsink.js
+++ cloudsink.js
@@ -5,7 +5,7 @@
 var readdirp = require('readdirp');
 var path = require('path');
 var cli = require('commander');
-var RacksJS = require('racksjs');
+var RacksJS = require('./../racksjs/dist/racks.js');
 var md5 = require('MD5');
 var fs = require('fs');
 var ratelimit = require('rate-limit');
@@ -40,10 +40,13 @@ if (!cli.region) {
 // Use a queue for GETs to prevent hitting API rate limits
 // Since we're uploading one at a time, it's not a big deal if this is a bit slow.
 // as most of the time this will be building out the UploadQueue well ahead of the upload() function
-var QuickQueue = ratelimit.createQueue({ interval: 500 });
+var GetQueue = ratelimit.createQueue({ interval: 500 });
 var container = false;
-
 var UploadQueue = [];
+var filesVerified = 0;
+var filesFound = 0;
+var newFiles = 0;
+var modifiedFiles = 0;
 
 new RacksJS({
   username: cli.username,
@@ -61,7 +64,10 @@ new RacksJS({
   // TODO: Verify container (option to create)
   container = rs.cloudFiles.containers.assume(cli.target);
 
-  container.listObjects(function (existingObjects) {
+  var readingDir = true;
+  console.log('Downloading list of remote objects...');
+  container.listAllObjects(function (existingObjects) {
+    console.log('Enumerating local directory & Compairing MD5 sums...');
     readdirp({ root: path.join(cli.source), fileFilter: cli.filter })
       .on('warn', function (err) {
         console.error('something went wrong when processing an entry', err);
@@ -71,6 +77,9 @@ new RacksJS({
       })
       .on('data', function (entry) {
         syncFile(entry, existingObjects);
+      })
+      .on('end', function () {
+        readingDir = false;
       });
   });
 
@@ -78,51 +87,69 @@ new RacksJS({
     uploadStarted = true;
     var file = UploadQueue.shift();
     if (file) {
-      var request = container.upload({
-        file: file
-      }, function (response) {
-        if (response.statusCode !== 201) {
-          console.log(file, 'Upload response code:', response.statusCode);
-        } else {
-          console.log('%s complete', file);
-          if (UploadQueue.length !== 0) {
-            upload();
+      // File may have been removed by now :P
+      if (!fs.existsSync(file)) {
+        upload();
+      } else {
+        var request = container.upload({
+          file: file
+        }, function (response) {
+          if (response.statusCode !== 201) {
+            console.log(file, 'Upload response code:', response.statusCode);
+          } else {
+            console.log('%s complete', file);
+            if (UploadQueue.length !== 0) {
+              upload();
+            }
           }
-        }
-      });
-      request.on('error', function (err) {
-        console.log('Connection error:', err);
-      });
+        });
+        request.on('error', function (err) {
+          console.log('Connection error:', err);
+        });
+      }
+    }
+  }
+  function startUploadCheck () {
+    filesVerified++;
+    if (filesVerified === filesFound && readingDir === false) {
+      console.log('%s files verified local & remote', filesFound);
+      console.log('%s files to upload', UploadQueue.length);
+      if (UploadQueue.length > 0) {
+        console.log('%s files are new, %s are different remotely', newFiles, modifiedFiles);
+        console.log('Beginning upload');
+        upload();
+      }
     }
   }
   function syncFile (entry, existingObjects) {
     var remoteFileName;
+    var localFileName;
     if (cli.source === '.') {
-      remoteFileName = entry.path;
+      localFileName = entry.path;
     } else {
-      remoteFileName = cli.source + path.sep + entry.path;
+      localFileName = cli.source + '/' + entry.path;
     }
+    filesFound++;
+    remoteFileName = localFileName.replace(path.sep, '/');
     // If remote file exists
     if (existingObjects.indexOf(remoteFileName) > -1) {
-      QuickQueue.add(function () {
+      GetQueue.add(function () {
         rs.get(container._racksmeta.target() + '/' + encodeURIComponent(remoteFileName), function (data, response) {
           var remoteMD5 = response.headers.etag;
           fs.readFile(remoteFileName, function(err, buf) {
             var localMD5 = md5(buf);
             if (remoteMD5 !== localMD5) {
+              modifiedFiles++;
               UploadQueue.push(remoteFileName);
-              if (!uploadStarted) {
-                upload();
-              }
             }
+            startUploadCheck();
           });
         });
       });
     } else {
+      newFiles++;
       UploadQueue.push(remoteFileName);
-      if (!uploadStarted) {
-        upload();
-      }
+      startUploadCheck();
     }
   }
 });
